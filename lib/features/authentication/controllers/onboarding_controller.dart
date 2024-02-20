e539283f7b5989ffa8e772e3367edfdd4e70680f
Awdhesh <awdheshdubey64@gmail.com>
2024-02-20 11:04:55 +0530

    Awdhesh

--- lib/features/authentication/controllers/onboarding_controller.dart
+++ lib/features/authentication/controllers/onboarding_controller.dart
@@ -1,43 +1,3 @@
-// //
-// // import 'package:flutter/cupertino.dart';
-// // import 'package:get/get.dart';
-// //
-// // import '../screens/login/login.dart';
-// //
-// // class OnBoardingController extends GetxController {
-// //   static OnBoardingController get instance => Get.find();
-// //
-// //   // variables
-// //   final pageController = PageController();
-// //   Rx<int> currentPageIndex = 0.obs;
-// //
-// //   // Update current Index when page Scroll
-// //   void updatePageIndicator(index) => currentPageIndex.value = index;
-// //
-// //   // Jump to the specific dot selected page
-// //   void dotNavigationClick(index) {
-// //     currentPageIndex.value = index;
-// //     pageController.jumpTo(index);
-// //   }
-// //
-// //   // Update current Index & Jump to next page
-// //   void nextPage() {
-// //     if (currentPageIndex.value == 2){
-// //        Get.to(const LoginScreen());
-// //     } else {
-// //       int page = currentPageIndex.value + 1;
-// //       pageController.jumpToPage(page);
-// //     }
-// //   }
-// //
-// //   // Update Current Index & Jump to the last page
-// //   void skipPage() {
-// //     currentPageIndex.value = 2;
-// //     pageController.jumpToPage(2);
-// //   }
-// //
-// // }
-//
 //
 // import 'package:flutter/cupertino.dart';
 // import 'package:get/get.dart';
@@ -63,7 +23,7 @@
 //   // Update current Index & Jump to next page
 //   void nextPage() {
 //     if (currentPageIndex.value == 2){
-//       Get.to(const LoginScreen());
+//        Get.to(const LoginScreen());
 //     } else {
 //       int page = currentPageIndex.value + 1;
 //       pageController.jumpToPage(page);
@@ -77,3 +37,44 @@
 //   }
 //
 // }
+
+
+
+import 'package:flutter/cupertino.dart';
+import 'package:get/get.dart';
+
+import '../screens/login/login.dart';
+
+class OnBoardingController extends GetxController {
+  static OnBoardingController get instance => Get.find();
+
+  // variables
+  final pageController = PageController();
+  Rx<int> currentPageIndex = 0.obs;
+
+  // Update current Index when page Scroll
+  void updatePageIndicator(index) => currentPageIndex.value = index;
+
+  // Jump to the specific dot selected page
+  void dotNavigationClick(index) {
+    currentPageIndex.value = index;
+    pageController.jumpTo(index);
+  }
+
+  // Update current Index & Jump to next page
+  void nextPage() {
+    if (currentPageIndex.value == 2){
+      Get.to(const LoginScreen());
+    } else {
+      int page = currentPageIndex.value + 1;
+      pageController.jumpToPage(page);
+    }
+  }
+
+  // Update Current Index & Jump to the last page
+  void skipPage() {
+    currentPageIndex.value = 2;
+    pageController.jumpToPage(2);
+  }
+
+}
