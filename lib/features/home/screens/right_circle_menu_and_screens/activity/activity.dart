f8c1d85aba35e966ab212965ee7952cb63880420
Awdhesh <awdheshdubey64@gmail.com>
2024-03-15 17:35:55 +0530

    Awdhesh

--- lib/features/home/screens/right_circle_menu_and_screens/activity/activity.dart
+++ lib/features/home/screens/right_circle_menu_and_screens/activity/activity.dart
@@ -1,105 +1,219 @@
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
+import 'package:globalcollegeapp/utils/constants/teext_styles.dart';
 import '../../../../../common/widgets/appbar/appbar.dart';
 import '../../../../../utils/constants/colors.dart';
-import 'controller/activity_controller.dart';
-
+import '../../../../../utils/constants/sizes.dart';
 
 class Activity extends StatelessWidget {
-  final ActivityController _controller = Get.put(ActivityController());
+  const Activity({Key? key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: EColors.backgroundColor,
-      appBar: const GAppBar(
-        surfaceTintColor: Colors.transparent,
+      appBar: GAppBar(
         backgroundColor: Colors.transparent,
         showBackArrow: true,
         centerTitle: false,
-        title: Text('Activities'),
+        title: Text(
+            'Activities',
+            style: TextStyleClass.appBarTextStyle
+        ),
       ),
-      body: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Column(
-            children: [
-              SingleChildScrollView(
-                physics: const BouncingScrollPhysics(),
-                child: Obx(() {
-                  return Column(
-                    children: [
-                      if (_controller.selectedIndex.value == 0)
-                        Container(
-                          color: Colors.cyan.shade50,
-                          child: const Text('First Contents'),
-                        ),
-                      if (_controller.selectedIndex.value == 1)
-                        Container(
-                          color: Colors.red.shade50,
-                          child: const Text('Second Contents'),
-                        ),
-                      if (_controller.selectedIndex.value == 2)
-                        Container(
-                          color: Colors.pink.shade50,
-                          child: const Text('Third Contents'),
-                        ),
-                    ],
-                  );
-                }),
-              ),
-            ],
-          ),
-
-          /// Vertical Menu Containers
-          SingleChildScrollView(
-            child: Column(
-              children: [
-                RotatedBox(
-                  quarterTurns: 1,
-                  child: Padding(
-                    padding: const EdgeInsets.only(top: 20, left: 20),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      body: DefaultTabController(
+        length: 5,
+        child: Column(
+          children: [
+            Flexible(
+              child: Row(
+                children: [
+                  Flexible(
+                    child: TabBarView(
                       children: [
-                        GestureDetector(
-                          onTap: () {
-                            _controller.setSelectedIndex(0);
-                          },
-                          child: Obx(() => Container(
-                            color: _controller.selectedIndex.value == 0
-                                ? Colors.white
-                                : Colors.transparent,
-                            child: const Text('First menu'),
-                          )),
+                        // Content for Category 1
+                        SingleChildScrollView(
+                          physics: const BouncingScrollPhysics(),
+                          child: Column(
+                            children: [
+                              // StackContainer(),
+                              //  ContainerStack(),
+                              SizedBox(
+                                height: 15,
+                              ),
+                              ClipRRect(
+                                borderRadius: BorderRadius.circular(20),
+                                child: Container(
+                                  height: 220,
+                                  width: 220,
+                                  color: Colors.red.shade50,
+                                ),
+                              ),
+                              SizedBox(
+                                height: 15,
+                              ),
+                              ClipRRect(
+                                borderRadius: BorderRadius.circular(20),
+                                child: Container(
+                                  height: 220,
+                                  width: 220,
+                                  color: Colors.blue.shade50,
+                                ),
+                              ),
+                              SizedBox(
+                                height: 15,
+                              ),
+                              ClipRRect(
+                                borderRadius: BorderRadius.circular(20),
+                                child: Container(
+                                  height: 220,
+                                  width: 220,
+                                  color: Colors.red.shade50,
+                                ),
+                              ),
+                            ],
+                          ),
                         ),
-                        const SizedBox(width: 20),
-                        GestureDetector(
-                          onTap: () {
-                            _controller.setSelectedIndex(1);
-                          },
-                          child: Obx(() => Container(
-                            color: _controller.selectedIndex.value == 1
-                                ? Colors.white
-                                : Colors.transparent,
-                            child: const Text('Second menu'),
-                          )),
+                        // Content for Category 2
+                        Stack(
+                          children: [
+                            Container(
+                              child: const Center(
+                                child: Text('Category 2 content'),
+                              ),
+                            ),
+                          ],
                         ),
-                        const SizedBox(width: 20),
-                        GestureDetector(
-                          onTap: () {
-                            _controller.setSelectedIndex(2);
-                          },
-                          child: Obx(() => Container(
-                            color: _controller.selectedIndex.value == 2
-                                ? Colors.white
-                                : Colors.transparent,
-                            child: const Text('Third menu'),
-                          )),
+                        // Content for Category 3
+                        Container(
+                          child: const Center(
+                            child: Text('Category 3 content'),
+                          ),
                         ),
+                        // Add more TabBarView children as needed
+                        Container(
+                          child: const Center(
+                            child: Text('Category 4 content'),
+                          ),
+                        ),
+                        // Add more TabBarView children as needed
+                        Container(
+                          child: const Center(
+                            child: Text('Category 5 content'),
+                          ),
+                        ),
+                        // Add more TabBarView children as needed
                       ],
                     ),
                   ),
+                  RotatedBox(
+                    quarterTurns: 9,
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Container(
+                        constraints: const BoxConstraints.expand(
+                            height: 50, width: double.maxFinite),
+                        child: const TabBar(
+                          isScrollable: true,
+                          dividerColor: Colors.transparent,
+                          indicator: DotTabIndicator(
+                            color: Colors.redAccent,
+                            radius: 4,
+                          ),
+                          tabs: [
+                            Padding(
+                              padding: EdgeInsets.all(8.0),
+                              child: Tab(text: 'Global Youth Fest 2023'),
+                            ),
+                            Padding(
+                              padding: EdgeInsets.all(8.0),
+                              child: Tab(text: 'Global College Yoga Day'),
+                            ),
+                            Padding(
+                              padding: EdgeInsets.all(8.0),
+                              child: Tab(text: 'Engineering'),
+                            ),
+                            Padding(
+                              padding: EdgeInsets.all(8.0),
+                              child: Tab(text: 'Category 4'),
+                            ),
+                            Padding(
+                              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                              child: Tab(text: 'Category 5'),
+                            ),
+                            // Add more tabs as needed
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class DotTabIndicator extends Decoration {
+  final Color color;
+  final double radius;
+
+  const DotTabIndicator({required this.color, required this.radius});
+
+  @override
+  _DotPainter createBoxPainter([VoidCallback? onChanged]) {
+    return _DotPainter(this, onChanged);
+  }
+}
+
+class _DotPainter extends BoxPainter {
+  final DotTabIndicator decoration;
+
+  _DotPainter(this.decoration, VoidCallback? onChanged) : super(onChanged);
+
+  @override
+  void paint(Canvas canvas, Offset offset, ImageConfiguration configuration) {
+    final Paint paint = Paint()..color = decoration.color;
+    final Rect rect = offset & configuration.size!;
+    final double centerX = rect.center.dx;
+    final double bottom = rect.bottom - 6; // Adjust the value based on your preference
+
+    canvas.drawCircle(Offset(centerX, bottom), decoration.radius, paint);
+  }
+}
+
+class ContainerStack extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 200, // Change this according to your requirement
+      height: 200, // Change this according to your requirement
+      child: Stack(
+        children: [
+          Container(
+            color: Colors.blue,
+            width: double.infinity,
+            height: double.infinity,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(
+                  width: 100, // Change this according to your requirement
+                  height: 100, // Change this according to your requirement
+                  color: Colors.green,
+                ),
+                Container(
+                  width: 100, // Change this according to your requirement
+                  height: 100, // Change this according to your requirement
+                  color: Colors.red,
+                ),
+                Container(
+                  width: 100, // Change this according to your requirement
+                  height: 100, // Change this according to your requirement
+                  color: Colors.yellow,
                 ),
               ],
             ),
@@ -109,6 +223,109 @@ class Activity extends StatelessWidget {
     );
   }
 }
+
+
+
+// import 'package:flutter/material.dart';
+// import 'package:get/get.dart';
+//
+// import '../../../../../common/widgets/appbar/appbar.dart';
+// import '../../../../../utils/constants/colors.dart';
+// import 'controller/activity_controller.dart';
+//
+// // Sample JSON data for menus and contents
+// const jsonData = '''
+// [
+//   {
+//     "menuTitle": "First menu",
+//     "contentColor": "cyan.shade50",
+//     "contentText": "First Contents"
+//   },
+//   {
+//     "menuTitle": "Second menu",
+//     "contentColor": "red.shade50",
+//     "contentText": "Second Contents"
+//   },
+//   {
+//     "menuTitle": "Third menu",
+//     "contentColor": "pink.shade50",
+//     "contentText": "Third Contents"
+//   }
+// ]
+// ''';
+//
+// class Activity extends StatelessWidget {
+//   final ActivityController _controller = Get.put(ActivityController());
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     final List<dynamic> menuData = json.decode(jsonData);
+//
+//     return Scaffold(
+//       backgroundColor: EColors.backgroundColor,
+//       appBar: const GAppBar(
+//         surfaceTintColor: Colors.transparent,
+//         backgroundColor: Colors.transparent,
+//         showBackArrow: true,
+//         centerTitle: false,
+//         title: Text('Activities'),
+//       ),
+//       body: Row(
+//         mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//         children: [
+//           Expanded(
+//             child: SingleChildScrollView(
+//               physics: const BouncingScrollPhysics(),
+//               child: Obx(() {
+//                 return Column(
+//                   children: List.generate(menuData.length, (index) {
+//                     final Map<String, dynamic> item = menuData[index];
+//                     return _controller.selectedIndex.value == index
+//                         ? Container(
+//                       color: Colors
+//                           .cyan
+//                           .shade50, // Use the color from JSON data
+//                       child: Text(item['contentText'] as String),
+//                     )
+//                         : SizedBox.shrink();
+//                   }),
+//                 );
+//               }),
+//             ),
+//           ),
+//           SingleChildScrollView(
+//             child: Column(
+//               children:
+//               List.generate(menuData.length, (index) {
+//                 final Map<String, dynamic> item = menuData[index];
+//                 return GestureDetector(
+//                   onTap: () {
+//                     _controller.setSelectedIndex(index);
+//                   },
+//                   child: Obx(() => Row(
+//                     children: [
+//                       RotatedBox(
+//                         quarterTurns: 1,
+//                         child: Container(
+//                           color: _controller.selectedIndex.value == index
+//                               ? Colors.white
+//                               : Colors.transparent,
+//                           child: Text(item['menuTitle'] as String),
+//                         ),
+//                       ),
+//                     ],
+//                   )),
+//                 );
+//               }),
+//             ),
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }
+//
+
 
 
 /*
