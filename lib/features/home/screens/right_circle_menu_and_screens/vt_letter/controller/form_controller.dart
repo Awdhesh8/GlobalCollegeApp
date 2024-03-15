0c019240147b988c0458701169fd72abe68eec44
Awdhesh Dubey <awdheshdubey64@gmail.com>
2024-03-15 16:33:11 +0530

    From Mac Git(SecondBranch) By alok

--- lib/features/home/screens/right_circle_menu_and_screens/vt_letter/controller/form_controller.dart
+++ lib/features/home/screens/right_circle_menu_and_screens/vt_letter/controller/form_controller.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:multi_dropdown/models/value_item.dart';
 
 import '../widgets/vt_letter_form.dart';
 
@@ -22,16 +21,17 @@ class VTLetterFormController extends GetxController {
   RxList<String> vtLetterHistory = <String>[].obs;
 
   Rx<List<DropdownMenuItem<String>>> listCompanyDropDown = Rx<List<DropdownMenuItem<String>>>([]);
-  Rx<List<ValueItem<String>>> companyValueItem = Rx<List<ValueItem<String>>>([]);
+  //Rx<List<ValueItem<String>>> companyValueItem = Rx<List<ValueItem<String>>>([]);
 
   void getCompany()async{
     listCompanyDropDown.value.clear();
     print(vtSubjectId);
-    listCompanyDropDown.value.add(DropdownMenuItem(child: Text("GCF"),value: '1998',));
-    listCompanyDropDown.value.add(DropdownMenuItem(child: Text("TCS"),value: '1948',));
+    //listCompanyDropDown.value.add(DropdownMenuItem(child: Text("GCF"),value: '1998',));
+    //listCompanyDropDown.value.add(DropdownMenuItem(child: Text("TCS"),value: '1948',));
+
     print(vtSubjectId);
 
-    companyValueItem.value.add(const ValueItem(label: 'label', value: '33'));
+    //companyValueItem.value.add(const ValueItem(label: 'label', value: '33'));
     //listCompanyDropDown.value.add(DropdownMenuItem(value: '0',child: Text(vtSubjectId.toString()),));
   }
 }
--- lib/features/home/screens/right_circle_menu_and_screens/vt_letter/widgets/vt_letter_form.dart
+++ lib/features/home/screens/right_circle_menu_and_screens/vt_letter/widgets/vt_letter_form.dart
@@ -272,23 +272,30 @@ class VTLetterForm extends StatelessWidget {
 
   }
 
-  Obx _showMultiSelectBox(BuildContext context){
+  MultiSelectDropDown _showMultiSelectBox(BuildContext context){
 
-    return Obx(() => MultiSelectDropDown(
+    return MultiSelectDropDown(
       //showClearIcon: true,
       //controller: controller.selectedCompanyww,
       onOptionSelected: (options) {
         print(options);
       },
-      options: controller.companyValueItem.value,
+      options: const <ValueItem>[
+        ValueItem(label: 'Option 1', value: '1'),
+        ValueItem(label: 'Option 2', value: '2'),
+        ValueItem(label: 'Option 3', value: '3'),
+        ValueItem(label: 'Option 4', value: '4'),
+        ValueItem(label: 'Option 5', value: '5'),
+        ValueItem(label: 'Option 6', value: '6'),
+      ],
       maxItems: 3,
-      //disabledOptions: const [ValueItem(label: 'Option 1', value: '1')],
+      disabledOptions: const [ValueItem(label: 'Option 1', value: '1')],
       selectionType: SelectionType.multi,
       chipConfig: const ChipConfig(wrapType: WrapType.wrap),
       dropdownHeight: 300,
       optionTextStyle: const TextStyle(fontSize: 16),
       selectedOptionIcon: const Icon(Icons.check_circle),
-    ),);
+    );
 
   }
 }
